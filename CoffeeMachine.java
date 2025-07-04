9580e891f96641e4fcf1a445d24600e5172b74fc
Pavan <loyola.pavan.74@gmail.com>
2025-07-04 09:52:04 +0530

    #2 challenge

---
+++ CoffeeMachine.java
@@ -0,0 +1,34 @@
+/*Automated Coffee Machine Startup with Repeated Humorous Message
+In this scenario, the automated coffee machine system displays a humorous message to the user when the machine starts up. The message should be printed three times, each on a new line, to emphasize the humor and make the interaction even more fun. The message to be displayed is: "I need coffee too!", repeated three times, each on a separate line.
+
+The system starts by printing the message "I need coffee too!" three times, each on a new line. This enhances the humor and adds a friendly and engaging element to the user experience, making the wait for coffee more entertaining.
+
+
+
+Input Format
+
+There is no input required for this program.
+
+
+
+Output Format
+
+The output should display the message thrice in new line
+
+
+
+Sample Output
+
+I need coffee too!!
+
+I need coffee too!!
+
+I need coffee too!! */
+
+public class CoffeeMachine {
+    public static void main(String[] args) {
+        System.out.println("I need coffee too!!");
+        System.out.println("I need coffee too!!");
+        System.out.println("I need coffee too!!");
+    }
+}
